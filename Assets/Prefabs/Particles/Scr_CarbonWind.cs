7022406c2b6b74f8262d64fb9620cf0655c6438a
Russ Painter <russp@geekymonkey.com>
2017-07-22 14:28:10 +0100

    Carbon emitter for spawning particles

---
+++ Assets/Prefabs/Particles/Scr_CarbonWind.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Scr_CarbonWind : MonoBehaviour
+{
+    public Vector2 WindDirection;
+    public float WindSpeed = 1;
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(WindDirection * WindSpeed);
+    }
+}
---
+++ Assets/Scr_CarbonEmitter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Scr_CarbonEmitter : MonoBehaviour
+{
+    public int StartQty = 4;
+    public float SecondsBetween = 1;
+    public Vector2 Range = new Vector2(5, 4);
+    public GameObject CarbonPrefab;
+
+    private float Reload;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < StartQty; i++)
+        {
+            Emit();
+        }
+    }
+
+    void Emit()
+    {
+        var newCarbon = GameObject.Instantiate(CarbonPrefab);
+        newCarbon.transform.position = new Vector3(UnityEngine.Random.Range(-Range.x, Range.x), UnityEngine.Random.Range(-Range.y, Range.y), newCarbon.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Reload += Time.deltaTime;
+        if (Reload > SecondsBetween)
+        {
+            Emit();
+            Reload = 0;
+        }
+
+    }
+}
